a48f63b5a7a2dce8bbfe12ef4e03b064f0b62ea7
Pauline Viroux <pilineviroux@hotmail.com>
2017-01-24 16:27:14 +0100

    Implement fastfood deletion

---
+++ src/controllers/fastfood/destroy.js
@@ -0,0 +1,36 @@
+/* paulineviroux/RIA/egzamen
+ *
+ * /src/controllers/fastfood/destroy.js - Delete fastfood controller
+ *
+ * coded by paulineviroux!
+ * started at 21/01/2017
+ */
+
+import { ObjectID } from "mongodb";
+import { send, error } from "../../core/utils/api.js";
+import getFastfood from "../../models/fastfood.js";
+
+export default function( oRequest, oResponse ) {
+
+    let oFastfoodID;
+
+    try {
+        oFastfoodID = new ObjectID( oRequest.params.id );
+    } catch( oError ) {
+        return error( oRequest, oResponse, new Error( "Invalid ID!" ), 400 );
+    }
+
+    getFastfood()
+        .deleteOne( {
+            "_id": oFastfoodID,
+        } )
+        .then( ( { deletedCount } ) => {
+            if ( deletedCount === 1 ) {
+                return send( oRequest, oResponse, null, 204 );
+            }
+
+            return error( oRequest, oResponse, "Unknown deletion error", 500 );
+        } )
+        .catch( ( oError ) => error( oRequest, oResponse, oError ) );
+
+}
--- src/routes/fastfood.js
+++ src/routes/fastfood.js
@@ -11,12 +11,14 @@ import { Router } from "express";
 import list from "../controllers/fastfood/list.js";
 import details from "../controllers/fastfood/details.js";
 import create from "../controllers/fastfood/create.js";
+import destroy from "../controllers/fastfood/destroy.js";
 
 let oRouter = new Router();
 
 oRouter.get( "/fastfood", list );
 oRouter.get( "/fastfood/:id", details );
 oRouter.post( "/fastfood", create );
+oRouter.delete( "/fastfood/:id", destroy );
 
 export default oRouter;
 
