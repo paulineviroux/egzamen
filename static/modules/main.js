30f9c20b0f7074a031ef0f207afa55415526ee7d
Pauline Viroux <pilineviroux@hotmail.com>
2017-01-24 18:20:40 +0100

    Install dependencies $ add base  modules

---
+++ static/modules/main.js
@@ -0,0 +1,13 @@
+/* paulineviroux/RIA/egzamen
+ *
+ * /static/modules/main.js - Main entry file
+ *
+ * coded by paulineviroux!
+ * started at 21/01/2017
+ */
+
+import utils from "./utils";
+
+console.log( "Hey!" );
+
+utils();
---
+++ static/modules/utils.js
@@ -0,0 +1,11 @@
+/* paulineviroux/RIA/egzamen
+ *
+ * /static/modules/utils.js - Utils
+ *
+ * coded by paulineviroux!
+ * started at 21/01/2017
+ */
+
+export default function() {
+    console.log( "Hey from utils" );
+}
