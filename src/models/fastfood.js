e0da200a668833389606575322fa5f2c1c65b32a
Pauline Viroux <pilineviroux@hotmail.com>
2017-01-24 16:49:29 +0100

    Update model for fastfood

--- src/models/fastfood.js
+++ src/models/fastfood.js
@@ -6,8 +6,43 @@
  * started at 21/01/2017
  */
 
+import Promise from "bluebird";
 import { db } from "../core/mongodb";
+import { ObjectID } from "mongodb";
+
+let fCheckFastfood;
+
+fCheckFastfood = function( sBankID ) {
+    let oFastfoodID;
+
+    if ( !sFastfoodID ) {
+        return Promise.resolve( false );
+    }
+
+    try {
+        oFastfoodID = new ObjectID( sFastfoodID );
+    } catch( oError ) {
+        return Promise.reject( new Error( "Invalid Bank ID!" ) );
+    }
+
+    return db.collection( "fastfood" )
+        .findOne( {
+            "_id": oFastfoodID,
+        } )
+        .then( ( oFastfood ) => {
+            if ( oFastfood ) {
+                return Promise.resolve( true );
+            }
+
+            return Promise.reject( new Error( "Unknown Bank!" ) );
+        } );
+};
+
 
 export default function() {
     return db.collection( "fastfood" );
 }
+
+export {
+    fCheckFastfood as checkFastfood,
+};
