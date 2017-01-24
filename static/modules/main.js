fa9a1301d29d45011da4dac0be71da42f13a1740
Pauline Viroux <pilineviroux@hotmail.com>
2017-01-24 18:51:37 +0100

    Toying with Vue

--- static/modules/main.js
+++ static/modules/main.js
@@ -9,8 +9,38 @@
 import Vue from "vue";
 
 let oApp = new Vue( {
-    "el": "#app",
+    "template": `
+        <div class="box">
+            <p>{{ message }}</p>
+            <ul>
+                <li v-for="cat in cats">
+                    <strong>{{ cat.name }}</strong>
+                    <span>( {{ cat.age }} )</span>
+                </li>
+            </ul>
+            <p v-if="secret">Im a cat person!</p>
+            <button v-on:click="revealSecret">{{ reveal.value }}</button>
+        </div>
+    `,
     "data": {
         "message": "Hey from Vue!",
+        "secret": false,
+        "cats": [
+            { "name": "Skitty", "age": 6 },
+            { "name": "Pixel", "age": 4 },
+        ],
+        "reveal": {
+            "show": "Reveal my secrets!",
+            "hide": "Hide my secrets!",
+            "value": "Reveal my secrets!",
+        },
+    },
+    "methods": {
+        "revealSecret": function() {
+            this.secret = !this.secret;
+            this.reveal.value = this.secret ? this.reveal.hide : this.reveal.show;
+        },
     },
 } );
+
+oApp.$mount( "#app" );
