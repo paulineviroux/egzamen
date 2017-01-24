17313b826d033db55ea72835bf4e70f29bb49d29
Pauline Viroux <pilineviroux@hotmail.com>
2017-01-24 11:39:37 +0100

    Implement model for collection

---
+++ src/models/fastfood.js
@@ -0,0 +1,13 @@
+/* paulineviroux/RIA/egzamen
+ *
+ * /src/models/fastfood.js - Model for fastfood
+ *
+ * coded by paulineviroux!
+ * started at 21/01/2017
+ */
+
+import { db } from "../core/mongodb";
+
+let oBanks = db.collection( "fastfood" );
+
+export default oFastfood;
